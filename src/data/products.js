3545adacd850ab2ac0eefad7407e084c6dd5727b
Demyan <dem4ik231213@gmail.com>
2026-03-04 15:44:01 +0200

    fix images

--- src/data/products.js
+++ src/data/products.js
@@ -1,52 +1,61 @@
+import pizza1 from "../assets/pizza1.jpg";
+import pizza2 from "../assets/pizza2.jpg";
+import burger from "../assets/burger.jpg";
+import salad from "../assets/salad.jpg";
+import cheesecake from "../assets/cheesecake.jpg";
+import pancake from "../assets/pancake.jpg";
+import salad2 from "../assets/salad 2.jpg";
+import salad3 from "../assets/salad3.jpg";
+
 export const products = [
   {
     id: 1,
-    name: "Піца “Барбекю Чікен” ",
+    name: 'Піца "Барбекю Чікен"',
     description: "курка • соус барбекю • моцарела",
     weight: "480 гр",
     price: 359,
     oldPrice: null,
-    image: "/src/assets/pizza1.jpg",
+    image: pizza1,
     badge: null,
   },
   {
     id: 2,
-    name: "Піца “4 Сири”",
+    name: 'Піца "4 Сири"',
     description: "сет 21",
     weight: "500 гр",
     price: 389,
     oldPrice: null,
-    image: "/src/assets/pizza2.jpg",
+    image: pizza2,
     badge: null,
   },
   {
     id: 3,
-    name: "Бургер “Classic Beef”",
+    name: 'Бургер "Classic Beef"',
     description: "Яловичина • сир чеддер",
     weight: "180 гр",
     price: 129,
     oldPrice: null,
-    image: "/src/assets/burger.jpg",
+    image: burger,
     badge: null,
   },
   {
     id: 4,
-    name: "Салат “Цезар з куркою”",
+    name: 'Салат "Цезар з куркою"',
     description: "Курка • яйце • пармезан",
     weight: "210 гр",
     price: 179,
     oldPrice: null,
-    image: "/src/assets/salad.jpg",
+    image: salad,
     badge: null,
   },
   {
     id: 5,
-    name: "Торт “Малиновий чизкейк”",
+    name: 'Торт "Малиновий чизкейк"',
     description: "Ніжний крем • малина",
     weight: "180 гр",
     price: 179,
     oldPrice: null,
-    image: "/src/assets/cheesecake.jpg",
+    image: cheesecake,
     badge: null,
   },
   {
@@ -56,7 +65,7 @@ export const products = [
     weight: "250 гр",
     price: 220,
     oldPrice: null,
-    image: "/src/assets/pancake.jpg",
+    image: pancake,
     badge: null,
   },
   {
@@ -66,7 +75,7 @@ export const products = [
     weight: "230 гр",
     price: 239,
     oldPrice: 289,
-    image: "/src/assets/salad 2.jpg",
+    image: salad2,
     badge: "🔥 Хіт",
   },
   {
@@ -76,7 +85,7 @@ export const products = [
     weight: "220 гр",
     price: 250,
     oldPrice: null,
-    image: "/src/assets/salad3.jpg",
+    image: salad3,
     badge: null,
   },
 ];
